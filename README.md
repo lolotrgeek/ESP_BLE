dfa09f61c71d3e1b38d6ff89d67037a95573985c
lolotrgeek <jon@simplyenable.com>
2021-06-04 20:45:37 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# ESP_BLE
+Scan for nearby BLE devices. Will turn into a room occupancy scanner.
---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+/*
+   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
+   Ported to Arduino ESP32 by Evandro Copercini
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+
+int scanTime = 5; //In seconds
+BLEScan* pBLEScan;
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      int rssi = advertisedDevice.getRSSI(); // mW
+      uint8_t* payload = advertisedDevice.getPayload();
+      Serial.printf("Advertised Device: %s, %s: %d \n", advertisedDevice.toString().c_str(), "Signal" , rssi);
+      // Serial.printf("Payload: %02x , %02x , %02x , %02x , %02x , %02x \n",
+      //    payload[0] & 0xff, payload[1] & 0xff, payload[2] & 0xff,
+      //    payload[3] & 0xff, payload[4] & 0xff, payload[5] & 0xff);
+      
+    }
+};
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Scanning...");
+
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);  // less or equal setInterval value
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  Serial.print("Devices found: ");
+  Serial.println(foundDevices.getCount());
+  Serial.println("Scan done!");
+  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+  delay(2000);
+}
